b6fd2fb369ec61fe9ff1440d11b36330dbc29a67
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-01 13:27:44 -0800

    Criando operacao de instanciacao

--- src/main/java/com/gcaldas/conceptualmodel/ConceptualmodelApplication.java
+++ src/main/java/com/gcaldas/conceptualmodel/ConceptualmodelApplication.java
@@ -1,13 +1,31 @@
 package com.gcaldas.conceptualmodel;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ConceptualmodelApplication {
+import com.gcaldas.conceptualmodel.domain.Category;
+import com.gcaldas.conceptualmodel.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class ConceptualmodelApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualmodelApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
