433ce23e7125442958a7819328945f77d3b4ff7f
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-01 12:00:01 -0800

    Testando a primeira classe de dominio - Category

--- src/main/java/com/gcaldas/conceptualmodel/resources/CategoryResource.java
+++ src/main/java/com/gcaldas/conceptualmodel/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.gcaldas.conceptualmodel.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gcaldas.conceptualmodel.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST it's working";
+	public List<Category> listar() {
+
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 }
