184e21f9d5608e6e18bd5bcd99b38e5a860f9873
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-01 11:44:41 -0800

    REST testado

---
+++ src/main/java/com/gcaldas/conceptualmodel/resources/CategoryResource.java
@@ -0,0 +1,15 @@
+package com.gcaldas.conceptualmodel.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "REST it's working";
+	}
+}
