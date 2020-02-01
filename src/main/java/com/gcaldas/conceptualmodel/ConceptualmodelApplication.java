ec83c3fd0e125a823aeebbb5f576d7d762ec1d29
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-01 11:34:06 -0800

    Projeto criado

---
+++ src/main/java/com/gcaldas/conceptualmodel/ConceptualmodelApplication.java
@@ -0,0 +1,13 @@
+package com.gcaldas.conceptualmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptualmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptualmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcaldas/conceptualmodel/ConceptualmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcaldas.conceptualmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptualmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
