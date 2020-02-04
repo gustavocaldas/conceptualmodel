deb762be14404cc27c3187549686b0f89be1e67d
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-03 20:32:36 -0800

    Inserting a new category using POST

--- src/main/java/com/gcaldas/conceptualmodel/resources/CategoryResource.java
+++ src/main/java/com/gcaldas/conceptualmodel/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.gcaldas.conceptualmodel.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gcaldas.conceptualmodel.domain.Category;
 import com.gcaldas.conceptualmodel.services.CategoryService;
@@ -21,5 +25,13 @@ public class CategoryResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Category object = service.find(id);
 		return ResponseEntity.ok().body(object);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
