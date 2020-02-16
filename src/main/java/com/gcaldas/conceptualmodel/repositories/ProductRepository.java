8ff284f0dcadef16bd3f5994bee77c0aba109765
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-15 20:03:20 -0800

    Search for orders by name and categories

--- src/main/java/com/gcaldas/conceptualmodel/repositories/ProductRepository.java
+++ src/main/java/com/gcaldas/conceptualmodel/repositories/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.gcaldas.conceptualmodel.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gcaldas.conceptualmodel.domain.Category;
 import com.gcaldas.conceptualmodel.domain.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Product> findDistinctByNameContainingAndCategoriesIn(String name, List<Category> categories, Pageable pageRequest);
 }
---
+++ src/main/java/com/gcaldas/conceptualmodel/services/ProductService.java
@@ -0,0 +1,39 @@
+package com.gcaldas.conceptualmodel.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.gcaldas.conceptualmodel.domain.Category;
+import com.gcaldas.conceptualmodel.domain.Product;
+import com.gcaldas.conceptualmodel.repositories.CategoryRepository;
+import com.gcaldas.conceptualmodel.repositories.ProductRepository;
+import com.gcaldas.conceptualmodel.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository rep;
+	
+	@Autowired
+	private CategoryRepository repCategory;
+
+	public Product find(Integer id) {
+		Optional<Product> obj = rep.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found. Id: " + id + ", Type: " + Product.class.getName()));
+	}
+	
+	public Page<Product> search(String name, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Category> categories = repCategory.findAllById(ids);
+		return rep.findDistinctByNameContainingAndCategoriesIn(name, categories, pageRequest);
+	}
+}
