2db3c9a9860baaa1e7499454c0f3885660d7e1a8
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-23 13:38:58 -0800

    Implementing Authorization

--- src/main/java/com/gcaldas/conceptualmodel/config/SecurityConfig.java
+++ src/main/java/com/gcaldas/conceptualmodel/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gcaldas.conceptualmodel.security.JWTAuthenticationFilter;
+import com.gcaldas.conceptualmodel.security.JWTAuthorizationFilter;
 import com.gcaldas.conceptualmodel.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
