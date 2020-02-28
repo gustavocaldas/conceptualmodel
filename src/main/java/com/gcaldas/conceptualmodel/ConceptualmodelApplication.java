f46dd30c687feef8db58f8ed6f59dfe029667781
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-02-27 17:53:23 -0800

    Sending image by endpoint

--- src/main/java/com/gcaldas/conceptualmodel/ConceptualmodelApplication.java
+++ src/main/java/com/gcaldas/conceptualmodel/ConceptualmodelApplication.java
@@ -1,17 +1,11 @@
 package com.gcaldas.conceptualmodel;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.gcaldas.conceptualmodel.services.S3Service;
-
 @SpringBootApplication
 public class ConceptualmodelApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualmodelApplication.class, args);
@@ -19,7 +13,5 @@ public class ConceptualmodelApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("C:\\temp\\fotos\\cat1.jpg");
-
 	}
 }
--- src/main/java/com/gcaldas/conceptualmodel/services/ClientService.java
+++ src/main/java/com/gcaldas/conceptualmodel/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.gcaldas.conceptualmodel.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gcaldas.conceptualmodel.domain.Address;
 import com.gcaldas.conceptualmodel.domain.City;
@@ -32,6 +34,9 @@ public class ClientService {
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
+	@Autowired
+	private S3Service s3Service;
+	
 	@Autowired
 	private ClientRepository rep;
 
@@ -108,5 +113,9 @@ public class ClientService {
 	private void updateData(Client newObj, Client obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
